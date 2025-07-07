377f084c09412300ca01bbaee2b21197ca00d1f0
NgunyiGachie <antogachie@gmail.com>
2025-07-07 11:59:45 +0300

    stage deployment

--- dashboard/app.py
+++ dashboard/app.py
@@ -63,4 +63,7 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 8050)),
+        debug=True)
